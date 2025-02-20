fe75b69101fe1ab0e2124f6215ae4ac146b5eb3f
Arios Gionaldi <ariosgionaldi@gmail.com>
2025-02-20 14:36:15 +0800

    perbaikan default link

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get('/', 'AuthController::login');
+
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::attemptLogin');
 $routes->post('register', 'AuthController::attemptRegister');
